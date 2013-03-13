0bc16a299629107f51a7ecc347d8a7a879c76ea1
Fabricio Zuardi <fabricio@fabricio.org>
2013-03-13 08:19:43 -0300

    testando o gihub hook

--- www/wp-content/themes/quequere_html5blank/quequere_home.php
+++ www/wp-content/themes/quequere_html5blank/quequere_home.php
@@ -6,7 +6,10 @@ get_header();
 <div id="mascote">
   <img src="<?php echo get_template_directory_uri(); ?>/img/dev_mascote_home.png" alt="Quequeré" />
   <div id="balao">
-    <q>As vezes</q>
+    <blockquote>
+      <p>Às vezes o tigre em mim se demonstra cruel como é próprio da espécie.</p>
+      <p>Outras, cochila ou se enrosca em afago emoliente, mas sempre tigre, disfarçado.</p>
+    </blockquote>
   </div>
 </div>
 <?php get_footer(); ?>
