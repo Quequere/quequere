9bc5e4acd832b15b0afd3714cb00b171deb1ee4f
Fabricio Zuardi <fabricio@fabricio.org>
2013-04-09 18:59:25 -0300

    home com as novas imagens do Gus

--- www/wp-content/themes/quequere_html5blank/quequere_home.php
+++ www/wp-content/themes/quequere_html5blank/quequere_home.php
@@ -4,8 +4,9 @@
 get_header();
 ?>
 <div id="mascote">
-  <img src="<?php echo get_template_directory_uri(); ?>/img/dev_mascote_home.png" alt="Quequeré" />
+  <img src="<?php echo get_template_directory_uri(); ?>/img/home_personagem.png" alt="Quequeré" />
   <div id="balao">
+    <div id="balao_rabo"></div>
     <blockquote>
       <p>Às vezes o tigre em mim se demonstra cruel como é próprio da espécie.</p>
       <p>Outras, cochila ou se enrosca em afago emoliente, mas sempre tigre, disfarçado.</p>
