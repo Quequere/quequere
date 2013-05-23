93e14b5309635c30062693bef8658e2d8d92c281
Fabricio Zuardi <fabricio@fabricio.org>
2013-05-23 01:46:21 -0300

    servicos (background)

--- www/wp-content/themes/quequere_html5blank/header.php
+++ www/wp-content/themes/quequere_html5blank/header.php
@@ -26,7 +26,7 @@
 if (in_array($post->post_name, array('contatos', 'jogos'))){
 	$logo_branco = "_branco";
 }
-if (in_array($post->post_name, array('home', 'contatos', 'sobre', 'transparencia','jogos'))):
+if (in_array($post->post_name, array('home', 'contatos', 'sobre', 'servicos', 'transparencia','jogos'))):
 ?>
 <div id="temporary-background"></div>
 <div id="temporary-background-2"></div>
