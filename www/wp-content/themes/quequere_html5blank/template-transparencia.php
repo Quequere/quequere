e58e1fb8c498a3cf925455d8486b4da8eed7d9c1
Fabricio Zuardi <fabricio@fabricio.org>
2013-05-23 19:10:43 -0300

    balao na transparencia

---
+++ www/wp-content/themes/quequere_html5blank/template-transparencia.php
@@ -0,0 +1,13 @@
+<?php /* Template Name: Transparencia Template */ ?>
+
+<?php get_header(); ?>
+
+<div id="mascote">
+</div>
+<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<div id="balao">
+  <h1><?php the_title(); ?></h1>
+  <?php the_content(); ?>
+</div>
+<?php endwhile; endif; ?>
+<?php get_footer(); ?>
